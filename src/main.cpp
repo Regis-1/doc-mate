736b61cbbe5f2137001ddd8c12f9ba480124a5b3
Regis-1 <jdudzinski101@gmail.com>
2025-03-04 19:29:09 +0100

    [application] Added agruments handling
    Implemented simple arguments handling with distinction of two major
    modes of the application. Those modes are: terminal mode and server
    mode. To run the server mode the user also needs to add the ip address
    for the created socket.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,55 @@
 #include "server.h"
+#include <unistd.h>
 #include <iostream>
 
-int main(int argc, char **argv) {
-  std::cout << "Hello in Doc-Mate...\n";
+struct Options {
+  bool serverMode {false};
+  std::string ip;
+  std::string indexDir;
+};
 
-  Server srv("127.0.0.1", 8089, 5);
-  srv.bindSocket();
-  srv.listenOnSocket();
+void parseArgs(int argc, char **argv, Options &opts) {
+  int opt;
+  while ((opt = getopt(argc, argv, "s:")) != -1) {
+    switch (opt) {
+    case 's':
+      std::cout << "1\n";
+      opts.serverMode = true;
+      std::cout << "2\n";
+      opts.ip = optarg;
+      std::cout << "3\n";
+      break;
+    default:
+      std::cerr << "Usage: " << argv[0] << " [-s ip] index_dir\n";
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  if (optind >= argc) {
+    std::cerr << "Expected argument after options\n"
+	      << "Usage: " << argv[0] << " [-s ip] index_dir\n";
+    exit(EXIT_FAILURE);
+  }
+
+  opts.indexDir = argv[optind];
+}
+
+int main(int argc, char **argv) {
+  Options options;
+  
+  std::cout << "Hello in Doc-Mate...\n";
+  parseArgs(argc, argv, options);
+
+  if (options.serverMode) {
+    // server mode
+    Server srv(options.ip, 8089, 5);
+    srv.bindSocket();
+    srv.listenOnSocket();
+  }
+  else {
+    // local / terminal mode
+    std::cout << "Going with terminal mode...\n";
+  }
 
   std::cout << "Finishing work...\n";
   return 0;
